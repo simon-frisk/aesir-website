118169424c74db58813f48f18440bcb584922c1a
Simon Frisk <simon@simonfrisk.com>
2020-10-28 10:15:14 +0100

    Add project thumbnails

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,7 @@ import { Client } from '../prismic-config'
 import Prismic from 'prismic-javascript'
 import TextContainer from '../components/TextContainer'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 export default function Projects(props) {
   const activeProjects = props.projects.filter(project => project.active)
@@ -31,13 +32,17 @@ Projects.getInitialProps = async ctx => {
 }
 
 const Project = ({ project }) => {
-  const textExtract = project.about[0].text.slice(0, 120).split(' ').slice(0, -1).join(' ').concat(' ...')
 
+  const textExtract = project.about[0].text.slice(0, 150).split(' ').slice(0, -1).join(' ').concat(' ...')
+  
   return (
     <Link href={`/projects/${project.name[0].text}`}>
       <div id='container'>
-        <h3 style={{margin: 0}}>{project.name[0].text}</h3>
-        <p style={{margin: 0}}>{textExtract}</p>
+        {project.thumbnail.url && <img src={project.thumbnail.url} />}
+        <div>
+          <h3>{project.name[0].text}</h3>
+          <p>{textExtract}</p>
+        </div>
         <style jsx>{`
           #container {
             box-shadow: 0 0 5px 0 #333;
@@ -46,6 +51,7 @@ const Project = ({ project }) => {
             margin-top: 15px;
             cursor: pointer;
             transition: transform .2s;
+            display: flex;
           }
 
           #container:hover {
@@ -54,6 +60,24 @@ const Project = ({ project }) => {
 
           #container:active {
             transform: scale(1.02);
+          }
+
+          #container > img {
+            width: 150px;
+            object-fit: cover;
+          }
+
+          #container > div {
+            margin: 5px;
+          }
+          
+          @media (max-width: 700px) {
+            #container > img {
+              width: 100%;
+            }
+            #container {
+              display: block;
+            }
           }
         `}</style>
       </div>

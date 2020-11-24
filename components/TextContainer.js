56420d32d1c0805d03358cebf19b346328c39d78
Simon Frisk <simon@simonfrisk.com>
2020-11-24 10:47:48 +0100

    Set image max width

--- components/TextContainer.js
+++ components/TextContainer.js
@@ -1,13 +1,12 @@
 export default ({ children, lowTopMargin, ...props }) => (
-  <div
-    {...props}
-    style={{
-      maxWidth: '700px',
-      width: '85%',
-      margin: `${lowTopMargin ? '5vh' : '15vh'} auto`,
-      ...props.style,
-    }}
-  >
+  <div {...props} id='container'>
     {children}
+    <style jsx>{`
+      #container {
+        max-width: 700px;
+        width: 85%;
+        margin: ${lowTopMargin ? '5vh' : '15vh'} auto;
+      }
+    `}</style>
   </div>
 )
